15de001eb531e0114942554a1ca02f34ce1cd898
Paco Torrado <developer@pacotorrado.com>
2020-07-11 19:58:32 +0200

    Initial commit

---
+++ WP-software-developer-theme.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: WP Software Developer Theme
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: paco
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
