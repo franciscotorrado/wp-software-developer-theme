9dfe7c205c79a2732e4ee90811534117b22c79a8
Paco Torrado <developer@pacotorrado.com>
2020-07-12 02:23:08 +0200

    Format code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
-const searchForm = document.querySelector('#searchform #s');
-searchForm.className += ' form-control mb-2';
-
-const searchButton = document.querySelector('#searchsubmit');
-searchButton.className += ' btn btn-primary btn-block';
+document.querySelector('#searchform #s').className += ' form-control mb-2';
+document.querySelector('#searchsubmit').className += ' btn btn-primary btn-block';
 
 const widgetsList = document.querySelector('.ptwpsoftdevtheme-widget ul');
 widgetsList.className += ' list-group';
 widgetsList.querySelectorAll('li')
-    .forEach(item => {item.className += 'list-group-item'});
+    .forEach(item => {
+        item.className += 'list-group-item'
+    });
