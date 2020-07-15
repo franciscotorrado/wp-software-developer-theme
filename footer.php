4d8b56cdcbfd258c02ccc497719b7eabe436c62c
Paco Torrado <developer@pacotorrado.com>
2020-07-15 23:16:28 +0200

    Update footer legend

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <footer class="container-fluid py-4 text-center text-light colors-dark">
-    <h5>asd fadsfd fadsfas fasdas dfasd</h5>
+    <h6>
+        <a href="https://github.com/pacotorrado/wp-software-developer-theme">Software Developer Theme</a>
+        - by <a href="https://pacotorrado.com">Paco Torrado</a>
+        - GPL v.3</h6>
 </footer>
 
 <?php wp_footer(); ?>
